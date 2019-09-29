c231cd0c2aa15e6cfc7abe7b1a0ad007aebc3ad9
MaheKarim <mahekarim@gmail.com>
2019-09-30 01:34:02 +0600

    Agent Block Create

--- resources/views/backend/_home.blade.php
+++ resources/views/backend/_home.blade.php
@@ -92,6 +92,8 @@
 											<i class="flaticon-twitter text-primary"></i>
 										</div>
 									</div>
+
+
 									<div class="col-7 col-stats">
 										<div class="numbers">
 											<p class="card-category">Followers</p>
@@ -106,84 +108,7 @@
 				@endadmin
 				<!-- Start For Agent Code -->
 				 @agent
-				<div class="row row-card-no-pd mt--2">
-						<div class="col-sm-6 col-md-3">
-							<div class="card card-stats card-round">
-								<div class="card-body ">
-									<div class="row">
-										<div class="col-5">
-											<div class="icon-big text-center">
-												<i class="flaticon-user-2 text-success"></i>
-											</div>
-										</div>
-										<div class="col-7 col-stats">
-											<div class="numbers">
-												<p class="card-category">Total Admin</p>
-												<h4 class="card-title"> 234 </h4>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-sm-6 col-md-3">
-							<div class="card card-stats card-round">
-								<div class="card-body ">
-									<div class="row">
-										<div class="col-5">
-											<div class="icon-big text-center">
-												<i class="flaticon-user-1 text-success"></i>
-											</div>
-										</div>
-										<div class="col-7 col-stats">
-											<div class="numbers">
-												<p class="card-category">Total Agent</p>
-												<h4 class="card-title">140$</h4>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-sm-6 col-md-3">
-							<div class="card card-stats card-round">
-								<div class="card-body">
-									<div class="row">
-										<div class="col-5">
-											<div class="icon-big text-center">
-												<i class="flaticon-error text-danger"></i>
-											</div>
-										</div>
-										<div class="col-7 col-stats">
-											<div class="numbers">
-												<p class="card-category">Errors</p>
-												<h4 class="card-title">23</h4>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-sm-6 col-md-3">
-							<div class="card card-stats card-round">
-								<div class="card-body">
-									<div class="row">
-										<div class="col-5">
-											<div class="icon-big text-center">
-												<i class="flaticon-twitter text-primary"></i>
-											</div>
-										</div>
-										<div class="col-7 col-stats">
-											<div class="numbers">
-												<p class="card-category">Followers</p>
-												<h4 class="card-title">+45K</h4>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
+				  @include('backend.agent.staticbar')
                 @endagent
 
 				<!-- End For Agent Code -->
