255fa3c19957da2c4b183649636a0ca938704cae
Mahekarim <mahekarim@gmail.com>
2019-10-17 03:02:45 +0600

    Update Error

--- resources/views/frontend/welcome.blade.php
+++ resources/views/frontend/welcome.blade.php
@@ -664,16 +664,18 @@
                             <a href="stores_01.html" class="btn btn-o btn-xs pos-a right-10 pos-tb-center">All Stores</a>
                         </header>
                         <div class="popular-stores-slider owl-slider" data-loop="true" data-autoplay="true" data-smart-speed="1000" data-autoplay-timeout="10000" data-margin="20" data-items="2" data-xxs-items="2" data-xs-items="2" data-sm-items="3" data-md-items="5" data-lg-items="6">
-                          @foreach ($dokans as $dokan)
+                                @foreach ($dokans as $dokan)
                             <div class="store-item t-center">
+                               
                                 <a href="store_single_01.html" class="panel is-block">
                                     <div class="embed-responsive embed-responsive-4by3">
                                         <div class="store-logo">
                                             {{-- <img src="{{ asset('storage') }}/{{ $dokans->dokan_image }}" alt=""> --}}
                                         </div>
                                     </div>
-                                <h6 class="store-name ptb-10">{{ $dokans->dokan_name }}</h6>
+                                <h6 class="store-name ptb-10">{{ $dokans->first()->dokan_name }}</h6>
                                 </a>
+                               
                             </div>
                             @endforeach
                             
