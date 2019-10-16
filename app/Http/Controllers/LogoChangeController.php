835101a09841acb79a14efa694f2db412bd3b847
Mahekarim <mahekarim@gmail.com>
2019-10-16 23:41:00 +0600

    LogoError Working

--- app/Http/Controllers/LogoChangeController.php
+++ app/Http/Controllers/LogoChangeController.php
@@ -15,43 +15,20 @@ class LogoChangeController extends Controller
         return view('backend.logo', $data);
     }
 
-    public function imageUploadPost(){
-        // $request->validate([
-        //     'logo' => 'required|max:3048',
-        //  ]);
+    public function imageUploadPost(Request $request){
+        $request->validate([
+            'logo' => 'required|max:3048'
+         ]);
 
-        // $image = '';
-        //  if($request->has('logo')){
-        //      $image = $request->file('logo')->store('logo');
-        //  }
-        //  $logochange = new LogoChange();
-        //  $logochange->logo = $image;
-        //  return $image;
-        //  session()->flash('success','Logo Created successfully!');
-        //  return redirect(route('logoChange'));
+        $image = '';
+         if($request->has('logo')){
+             $image = $request->file('logo')->store('logo');
+         }
+         $logochange = new LogoChange();
+         $logochange->logo = $image;
+         $logochange->save();
+         session()->flash('success','Logo Created successfully!');
+         return redirect(route('logoChange'));
 
-    //     request()->validate([
-
-    //         'logo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-
-    //     ]);
-
-  
-
-    //   //  $logo = time().'.'.request()->image->getClientOriginalExtension();
-
-  
-
-    //     request()->image->move(public_path('logo'), $logo);
-
-  
-
-        return back()
-
-            ->with('success','You have successfully upload image.')
-
-            ->with('image',$logo);
-
-    }
-    
+        }
 }
